0d70dd4a38396d086f5b77e301e18129e0f271a1
Eneko Alonso <eneko.alonso@gmail.com>
2016-02-25 13:57:40 -0800

    Initial Commit

---
+++ Sync/Sync.h
@@ -0,0 +1,19 @@
+//
+//  Sync.h
+//  Sync
+//
+//  Created by Eneko Alonso on 2/25/16.
+//  Copyright © 2016 Eneko Alonso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sync.
+FOUNDATION_EXPORT double SyncVersionNumber;
+
+//! Project version string for Sync.
+FOUNDATION_EXPORT const unsigned char SyncVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sync/PublicHeader.h>
+
+
